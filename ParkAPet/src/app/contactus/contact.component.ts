bbae8f7fba8ce62f49046655332b23c283e7e844
Adam Jayne <adamjayne402@gmail.com>
2017-08-16 15:52:41 -0400

    fixed merge errors as well as Emiliya's routing issues

--- ParkAPet/src/app/contactus/contact.component.ts
+++ ParkAPet/src/app/contactus/contact.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { Router } from '@angular/router';
 // import {Message} from '../post.model';
 @Component({
     selector: 'contactus',
@@ -17,10 +18,12 @@ export class ContactUs {
     // petList: Post[] = [
     //     new Post('Test Pet', 'this is just a path', 'https://d2wq73xazpk036.cloudfront.net/media/27FB7F0C-9885-42A6-9E0C19C35242B5AC/A5F4E80F-72B7-458F-A40EB676E963E9A9/thul-1e3a85be-5590-5ef4-b332-bc456353498e.jpg?response-content-disposition=inline')
     // ];
-    constructor(){
+    constructor(
+        private router: Router
+    ){
         // console.log(this.petList);
     }
     onSubmit() {
-        
+        this.router.navigateByUrl('submit');
     }
 }
