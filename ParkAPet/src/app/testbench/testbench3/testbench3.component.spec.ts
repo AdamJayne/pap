2e0144c2bec83b95a56a48f87e21fe2553e31ce2
emiliya akmayeva <emiliya@lindholm.org>
2017-08-21 11:45:17 -0400

    commit contact changes

--- ParkAPet/src/app/testbench/testbench3/testbench3.component.spec.ts
+++ ParkAPet/src/app/testbench/testbench3/testbench3.component.spec.ts
@@ -1,49 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-
-
 import { Testbench3Component } from './testbench3.component';
 
-
-
 describe('Testbench3Component', () => {
-
   let component: Testbench3Component;
-
   let fixture: ComponentFixture<Testbench3Component>;
 
-
-
   beforeEach(async(() => {
-
     TestBed.configureTestingModule({
-
       declarations: [ Testbench3Component ]
-
     })
-
     .compileComponents();
-
   }));
 
-
-
   beforeEach(() => {
-
     fixture = TestBed.createComponent(Testbench3Component);
-
     component = fixture.componentInstance;
-
     fixture.detectChanges();
-
   });
-
-
 
   it('should be created', () => {
-
     expect(component).toBeTruthy();
-
   });
-
 });
--- ParkAPet/src/app/testbench/testbench3/testbench3.component.ts
+++ ParkAPet/src/app/testbench/testbench3/testbench3.component.ts
@@ -1,47 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
 import { PostService } from '../_services/post.service';
-
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
-
-
 @Component({
-
   selector: 'app-testbench3',
-
   templateUrl: './testbench3.component.html',
-
   styleUrls: ['./testbench3.component.css']
-
 })
-
 export class Testbench3Component implements OnInit {
-
   // items: FirebaseListObservable<any[]>;
-
   items;//: any = [];
-
   constructor(public pstSvc: PostService,
-
               public afd: AngularFireDatabase) {
-
                 // this.items = afd.list('/post');
-
                 // console.log(this.items)
-
                 
-
   }
-
-
 
   ngOnInit() {
-
     this.items = [];
-
     this.items = this.pstSvc.getAll();
-
   }
-
 }
