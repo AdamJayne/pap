57936bfc73bb0075f1e727671027aec07c5acc66
Elena Hickey <subhickeyelena@gmail.com>
2017-08-21 09:36:02 -0400

    added login with google

--- ParkAPet/src/app/login/login.component.ts
+++ ParkAPet/src/app/login/login.component.ts
@@ -38,6 +38,8 @@ export class LoginComponent implements OnInit {
 
 
     sociallogin() {
-        this.afa.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+        this.afa.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
     }
+
+    
 }
